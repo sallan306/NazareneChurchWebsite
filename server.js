71ae893804395e1e5ef0fe88f0d4fa56b41f5dd6
sallan306 <sallan306@gmail.com>
2020-03-09 02:20:54 -0700

    server had an error

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.post("/upload", (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
-
+}
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
