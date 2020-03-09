6684871570ec1f7c8e80f3e0d342d21d9635b3ab
sallan306 <sallan306@gmail.com>
2020-03-09 02:02:09 -0700

    might have had wrong package-lock files

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-const path = require('path')
-const express = require('express');
-const fileUpload = require ('express-fileupload')
+const path = require("path");
+const express = require("express");
+const fileUpload = require("express-fileupload");
 const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
-
+const port = process.env.PORT || 8080;
+const publicPath = path.join(__dirname, "..", "public");
 
 app.use(express.static(publicPath));
 app.use(fileUpload());
-app.use(require('prerender-node'));
+app.use(require("prerender-node"));
+app.get("/api/getList", (req, res) => {
+  var list = ["item1", "item2", "item3"];
+  res.json(list);
+  console.log("Sent list of items");
+});
+app.post("/upload", (req, res) => {
+  if (req.files == null) {
+    return res.status(400).json({ msg: "no file was uploaded" });
+  }
 
-app.post('/upload', (req, res) => {
-    if(req.files == null) {
-        return res.status(400).json({msg: "no file was uploaded"})
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(error);
+      return res.status(500).send(err);
     }
 
-    const file = req.files.file
-
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-        if(err) {
-            console.error(error);
-            return res.status(500).send(err)
-
-        }
-
-        res.json({ fileName: file.name, filePath: `/uploads/${file.name}`})
-    })
-})
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
-app.listen(port, ()=> console.log(`Server is up on port ${port}!`))
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+app.listen(port, () => console.log(`Server is up on port ${port}!`));
