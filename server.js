b4754574c77b2635a36d9ea1876aa35d2df6c286
sallan306 <sallan306@gmail.com>
2020-03-11 16:56:25 -0700

    wasn't in the right folder

--- server.js
+++ server.js
@@ -4,40 +4,63 @@ const fileUpload = require("express-fileupload");
 const app = express();
 const port = process.env.PORT || 8080;
 const publicPath = path.join(__dirname, "..", "public");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+
+var list = [
+  {
+    key: 1,
+    name: "birthday",
+    date: "wednesday, march 5",
+    time: "5PM",
+    imgURL: "https://via.placeholder.com/500x300",
+    description:
+      "This is the description for the event. blah blah blah. blah blah blah. blah blah blah. blah blah blah. blah blah blah."
+  },
+  {
+    key: 2,
+    name: "birthday",
+    date: "wednesday, march 5",
+    time: "5PM",
+    imgURL: "https://via.placeholder.com/500x300",
+    description:
+      "This is the description for the event. blah blah blah. blah blah blah. blah blah blah. blah blah blah. blah blah blah."
+  },
+  {
+    key: 3,
+    name: "birthday",
+    date: "wednesday, march 5",
+    time: "5PM",
+    imgURL: "https://via.placeholder.com/500x300",
+    description:
+      "This is the description for the event. blah blah blah. blah blah blah. blah blah blah. blah blah blah. blah blah blah."
+  }
+];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/static', express.static(path.join(__dirname, 'client/build')));
+app.use("/static", express.static(path.join(__dirname, "client/build")));
 app.use(fileUpload());
 app.use(require("prerender-node"));
+
 app.get("/api/getList", (req, res) => {
-  var list = ["item1", "item2", "item3"];
   res.json(list);
-  console.log("Sent list of items");
+  console.log("Sent list of items", list);
 });
 
-app.post("/upload", (req, res) => {
-  if (req.files == null) {
-    return res.status(400).json({ msg: "no file was uploaded" });
-  }
-
-  const file = req.files.file;
-
-  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-    if (err) {
-      console.error(error);
-      return res.status(500).send(err);
-    }
-
-    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-  });
+app.post("/api/addListItem", req => {
+  list.push(req.body);
+  console.log(req.body);
 });
-if (process.env.NODE_ENV === 'production') {
+
+app.post("/api/deleteListItem", (req, res) => {
+  list.pop(req.body.index);
+  console.log("deleted list item at index: " + req.body.index);
+});
+if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 app.listen(port, () => console.log(`Server is up on port ${port}!`));
